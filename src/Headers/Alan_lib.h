dcd4b51de0fae36d5af771f02cdda0607d5cb9d4
Nianzhi <Nianzhi.com>
2025-02-10 19:11:09 -0500

    create File IO and bumpallocator

--- src/Headers/Alan_lib.h
+++ src/Headers/Alan_lib.h
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <stdio.h>
+
+// This is to get malloc
+#include<stdlib.h>
+
+#include <string.h>
+
+#include <sys/stat.h>
 // #############################################################################
 //                           Defines
 // #############################################################################
@@ -12,6 +19,7 @@
 // #############################################################################
 //                           Logging
 // #############################################################################
+
 enum TextColor
 {
     TEXT_COLOR_BLACK,
@@ -77,3 +85,193 @@ void _log(char* prefix, char* msg, TextColor textColor,Args... args)
     SM_ERROR("Assertion HIT!");   \
   }                               \
 }
+// #############################################################################
+//                           Bump Allocator
+// #############################################################################
+
+struct BumpAllocator
+{
+    size_t size;
+    size_t used;
+    char* memory;
+};
+
+BumpAllocator make_bump_allocator(size_t size, void* memory)
+{
+    BumpAllocator result = {};
+    result.memory=(char*) malloc(szie);
+    if(result.memory)
+    {
+        result.size
+        memset(result.memory,0,result.size);// sets the memory to 0
+    }
+    else
+    {
+        SM_ASSERT(false,"Failed to allocate memory!");
+    }
+
+    return result;
+}
+
+
+char* bump_alloc(BumpAllocator* bumpAllocator, size_t size)
+{
+    char* result = nullptr;
+    size_t allignedSize=(size+ 7) & ~7; // this makes sure the first 4 bits are 0
+    if(bumpAllocator->used + allignedSize <= bumpAllocator->capacity)
+    {
+        result = bumpAllocator->memory + bumpAllocator->used;
+        bumpAllocator->used += allignedSize;
+    }
+    else
+    {
+        SM_ASSERT(false,"BumpAllocator is full!");
+    }
+
+    return result;
+}
+
+
+
+// #############################################################################
+//                           File I/O
+// #############################################################################
+
+long long get_timestamp(const char* file)
+{
+  struct stat file_stat = {};
+  stat(file, &file_stat);
+  return file_stat.st_mtime;
+}
+
+bool file_exists(const char* filePath)
+{
+  SM_ASSERT(filePath, "No filePath supplied!");
+
+  auto file = fopen(filePath, "rb");
+  if(!file)
+  {
+    return false;
+  }
+  fclose(file);
+
+  return true;
+}
+
+long get_file_size(const char* filePath)
+{
+  SM_ASSERT(filePath, "No filePath supplied!");
+
+  long fileSize = 0;
+  auto file = fopen(filePath, "rb");
+  if(!file)
+  {
+    SM_ERROR("Failed opening File: %s", filePath);
+    return 0;
+  }
+
+  fseek(file, 0, SEEK_END);
+  fileSize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  fclose(file);
+
+  return fileSize;
+}
+/*
+* Reads a file into a supplied buffer. We manage our own
+* memory and therefore want more control over where it 
+* is allocated
+*/
+char* read_file(const char* filePath, int* fileSize, char* buffer)
+{
+  SM_ASSERT(filePath, "No filePath supplied!");
+  SM_ASSERT(fileSize, "No fileSize supplied!");
+  SM_ASSERT(buffer, "No buffer supplied!");
+
+  *fileSize = 0;
+  auto file = fopen(filePath, "rb");
+  if(!file)
+  {
+    SM_ERROR("Failed opening File: %s", filePath);
+    return nullptr;
+  }
+
+  fseek(file, 0, SEEK_END);
+  *fileSize = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  memset(buffer, 0, *fileSize + 1);
+  fread(buffer, sizeof(char), *fileSize, file);
+
+  fclose(file);
+
+  return buffer;
+}
+
+char* read_file(const char* filePath, int* fileSize, BumpAllocator* bumpAllocator)
+{
+  char* file = nullptr;
+  long fileSize2 = get_file_size(filePath);
+
+  if(fileSize2)
+  {
+    char* buffer = bump_alloc(bumpAllocator, fileSize2 + 1);
+
+    file = read_file(filePath, fileSize, buffer);
+  }
+
+  return file; 
+}
+void write_file(const char* filePath, char* buffer, int size)
+{
+  SM_ASSERT(filePath, "No filePath supplied!");
+  SM_ASSERT(buffer, "No buffer supplied!");
+  auto file = fopen(filePath, "wb");
+  if(!file)
+  {
+    SM_ERROR("Failed opening File: %s", filePath);
+    return;
+  }
+
+  fwrite(buffer, sizeof(char), size, file);
+  fclose(file);
+}
+
+bool copy_file(const char* fileName, const char* outputName, char* buffer)
+{
+  int fileSize = 0;
+  char* data = read_file(fileName, &fileSize, buffer);
+
+  auto outputFile = fopen(outputName, "wb");
+  if(!outputFile)
+  {
+    SM_ERROR("Failed opening File: %s", outputName);
+    return false;
+  }
+
+  int result = fwrite(data, sizeof(char), fileSize, outputFile);
+  if(!result)
+  {
+    SM_ERROR("Failed opening File: %s", outputName);
+    return false;
+  }
+  
+  fclose(outputFile);
+
+  return true;
+}
+bool copy_file(const char* fileName, const char* outputName, BumpAllocator* bumpAllocator)
+{
+  char* file = 0;
+  long fileSize2 = get_file_size(fileName);
+
+  if(fileSize2)
+  {
+    char* buffer = bump_alloc(bumpAllocator, fileSize2 + 1);
+
+    return copy_file(fileName, outputName, buffer);
+  }
+
+  return false;
+}
+
